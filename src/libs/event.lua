37de48811a2efe0d6fb9c7ad48904e6c0eebabaf
CaveeJohnson <caveejohnson@hotmail.com>
2016-01-16 17:13:02 +0000

    event lib

---
+++ src/libs/event.lua
@@ -0,0 +1,31 @@
+event = {}
+local listeners = {}
+
+function event.listen(eventName, identifier, callback)
+  if not listeners[eventName] then
+    listeners[eventName] = {}
+  end
+
+  listeners[eventName][identifier] = callback
+end
+
+function event.remove(eventName, identifier)
+  if not listeners[eventName] then return end
+
+  listeners[eventName][identifier] = nil
+end
+
+function event.call(eventName, ...)
+  if not listeners[eventName] then return end
+
+  local a, b, c, d, e, f, g, h
+  for identifier, callback in pairs(listeners[eventName]) do
+    a, b, c, d, e, f, g, h = callback(...)
+  end
+
+  return a, b, c, d, e, f, g, h
+end
+
+function event.getTable()
+  return listeners
+end
